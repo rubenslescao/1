b0fe7de54b3f4bda82526caeb067d4d36f51b819
HUBI006 <hubi006@HUBI006s-MacBook-Pro.local>
2025-12-07 18:22:50 +0100

    🔧 Config Next.js: autoriser images Wikimedia Commons

--- excellence-francaise/next.config.js
+++ excellence-francaise/next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/wikipedia/commons/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
-
